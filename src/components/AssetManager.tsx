e29f8e87def332e97226c9acb8071d9cc0db4e92
dihbgs <dihborgesdr@gmail.com>
2024-01-27 20:52:04 -0300

    feat: create asset manager for handling imports

---
+++ src/components/AssetManager.tsx
@@ -0,0 +1,9 @@
+import { List } from "./icons/List";
+import { Home } from "./icons/Home";
+import { Search } from "./icons/Search";
+import { Library } from "./icons/Library";
+import { HomeActive } from "./icons/HomeActive";
+import { ChevronLeft } from "./icons/ChevronLeft";
+import { ChevronRight } from "./icons/ChevronRight";
+
+export { Home, ChevronLeft, ChevronRight, HomeActive, Library, Search, List };
