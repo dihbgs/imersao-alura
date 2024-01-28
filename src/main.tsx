8aaf3ad0b0dd0bef5f171182d12b5fbf3260de0a
dihbgs <dihborgesdr@gmail.com>
2024-01-28 12:06:09 -0300

    feat: add mobile media query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import React from 'react'
+import ReactDOM from "react-dom/client";
+import "./styles/media/query.css";
+import App from "./App.tsx";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
