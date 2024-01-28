c6f02c8fdf3d0d92cf38f6184e85a5c5da335720
dihbgs <dihborgesdr@gmail.com>
2024-01-27 21:27:51 -0300

    feat: create card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,18 @@
+import "../styles/Card.css";
+
+interface CardProps {
+  title: string;
+  image: string;
+  color: string;
+}
+
+const Card = ({ title, image, color }: CardProps) => {
+  return (
+    <div className="Card" style={{ backgroundColor: color }}>
+      <h2>{title}</h2>
+      <img src={image} alt={title} />
+    </div>
+  );
+};
+
+export default Card;
